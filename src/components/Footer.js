94dda38d58234ac699ba7541515f54aea19a487e
Jharby Saravia <jharbysaravia06@gmail.com>
2025-07-21 21:06:59 -0400

    feat: updated the logo of the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,144 +1,146 @@
-import emailjs from "emailjs-com";
+import emailjs from 'emailjs-com';
 import React from 'react';
 import './Footer.css';
-import logo from '../images/Lolito.webp'
+import logo from '../images/EDUARDO.svg';
 
 const ContactSectionFooter = () => {
-
   const handleEmailSubmit = (event) => {
     event.preventDefault();
 
-    emailjs.sendForm(
-      "service_xuzwmzb", 
-      "template_9ju698c", 
-      event.target,
-      "FJVzrb4_3M5vuIRd5" 
-
-    )
-    .then(() => {
-      alert("Message sent successfully!");
-    })
-    .catch((error) => {
-      console.error("Error sending message.", error);
-      alert("Failed to send message. Please try again.");
-    });
+    emailjs
+      .sendForm(
+        'service_xuzwmzb',
+        'template_9ju698c',
+        event.target,
+        'FJVzrb4_3M5vuIRd5'
+      )
+      .then(() => {
+        alert('Message sent successfully!');
+      })
+      .catch((error) => {
+        console.error('Error sending message.', error);
+        alert('Failed to send message. Please try again.');
+      });
 
     event.target.reset();
-  }
+  };
   return (
-    <section id="contact-1392">
-      <div className="cs-container">
-        <div className="cs-content">
-          <span className="cs-topper">Contact</span>
-          <h2 className="cs-title">Get in Touch</h2>
-          <p className="cs-text">
-          Contact me today for a fair and transparent quote for your pool. I’m committed to providing you with the best value, without any hidden fees or surprises          </p>
-          <ul className="cs-ul">
-            <li className="cs-li">
-              <picture className="cs-icon-wrapper">
+    <section id='contact-1392'>
+      <div className='cs-container'>
+        <div className='cs-content'>
+          <span className='cs-topper'>Contact</span>
+          <h2 className='cs-title'>Get in Touch</h2>
+          <p className='cs-text'>
+            Contact me today for a fair and transparent quote for your pool. I’m
+            committed to providing you with the best value, without any hidden
+            fees or surprises{' '}
+          </p>
+          <ul className='cs-ul'>
+            <li className='cs-li'>
+              <picture className='cs-icon-wrapper'>
                 <img
-                  aria-hidden="true"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/phone-76.svg"
-                  alt="phone icon"
-                  className="cs-icon"
-                  width="40"
-                  height="40"
-                  decoding="async"
+                  aria-hidden='true'
+                  src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/phone-76.svg'
+                  alt='phone icon'
+                  className='cs-icon'
+                  width='40'
+                  height='40'
+                  decoding='async'
                 />
               </picture>
-              <div className="cs-flex-group">
-                <span className="cs-header">Phone</span>
-                <a href="tel:786-545-6534" className="cs-link">
+              <div className='cs-flex-group'>
+                <span className='cs-header'>Phone</span>
+                <a href='tel:786-545-6534' className='cs-link'>
                   +786 545 6534
                 </a>
               </div>
             </li>
-            <li className="cs-li">
-              <picture className="cs-icon-wrapper">
+            <li className='cs-li'>
+              <picture className='cs-icon-wrapper'>
                 <img
-                  aria-hidden="true"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/mail-76.svg"
-                  alt="address icon"
-                  className="cs-icon"
-                  width="40"
-                  height="40"
-                  decoding="async"
+                  aria-hidden='true'
+                  src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/mail-76.svg'
+                  alt='address icon'
+                  className='cs-icon'
+                  width='40'
+                  height='40'
+                  decoding='async'
                 />
               </picture>
-              <div className="cs-flex-group">
-                <span className="cs-header">Email</span>
-                <a href="mailto:eduardo.diaz08@yahoo.es" className="cs-link">
+              <div className='cs-flex-group'>
+                <span className='cs-header'>Email</span>
+                <a href='mailto:eduardo.diaz08@yahoo.es' className='cs-link'>
                   eduardo.diaz08@yahoo.es
                 </a>
               </div>
             </li>
-            <li className="cs-li">
-              <picture className="cs-icon-wrapper">
+            <li className='cs-li'>
+              <picture className='cs-icon-wrapper'>
                 <img
-                  aria-hidden="true"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/pin-76.svg"
-                  alt="address icon"
-                  className="cs-icon"
-                  width="40"
-                  height="40"
-                  decoding="async"
+                  aria-hidden='true'
+                  src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/pin-76.svg'
+                  alt='address icon'
+                  className='cs-icon'
+                  width='40'
+                  height='40'
+                  decoding='async'
                 />
               </picture>
-              <div className="cs-flex-group">
-                <span className="cs-header">Servicing</span>
-                <p  className="cs-link">
-                Pinecrest, Cutler Bay, Coral Way, Coral Gables, South Miami, Westchester, Kendall, Homestead
+              <div className='cs-flex-group'>
+                <span className='cs-header'>Servicing</span>
+                <p className='cs-link'>
+                  Pinecrest, Cutler Bay, Coral Way, Coral Gables, South Miami,
+                  Westchester, Kendall, Homestead
                 </p>
               </div>
             </li>
           </ul>
         </div>
-        <form className="cs-form" onSubmit={handleEmailSubmit}>
-          <h3 className="cs-h3">Make Appointment</h3>
-          <label className="cs-label">
+        <form className='cs-form' onSubmit={handleEmailSubmit}>
+          <h3 className='cs-h3'>Make Appointment</h3>
+          <label className='cs-label'>
             Name
             <input
-              className="cs-input"
+              className='cs-input'
               required
-              type="text"
-              id="name-1392"
-              name="name"
-              placeholder="Name"
+              type='text'
+              id='name-1392'
+              name='name'
+              placeholder='Name'
             />
           </label>
-          <label className="cs-label cs-email">
+          <label className='cs-label cs-email'>
             Email
             <input
-              className="cs-input"
+              className='cs-input'
               required
-              type="email"
-              id="email-1392"
-              name="email"
-              placeholder="Email"
+              type='email'
+              id='email-1392'
+              name='email'
+              placeholder='Email'
             />
           </label>
-          <label className="cs-label cs-phone">
+          <label className='cs-label cs-phone'>
             Phone
             <input
-              className="cs-input"
+              className='cs-input'
               required
-              type="number"
-              id="phone-1392"
-              name="phone"
-              placeholder="Phone"
+              type='number'
+              id='phone-1392'
+              name='phone'
+              placeholder='Phone'
             />
           </label>
-          <label className="cs-label">
+          <label className='cs-label'>
             Message
             <textarea
-              className="cs-input cs-textarea"
+              className='cs-input cs-textarea'
               required
-              name="Message"
-              id="message-1392"
-              placeholder="Write message..."
-            ></textarea>
+              name='Message'
+              id='message-1392'
+              placeholder='Write message...'></textarea>
           </label>
-          <button className="cs-button-solid cs-submit" type="submit">
+          <button className='cs-button-solid cs-submit' type='submit'>
             Send Message
           </button>
         </form>
@@ -149,77 +151,95 @@ const ContactSectionFooter = () => {
 
 const Footer = () => {
   return (
-    <footer id="footer-1392">
-      <div className="cs-container">
-        <div className="cs-top">
-          <a aria-label="go back to home" className="cs-logo" href="/home">
+    <footer id='footer-1392'>
+      <div className='cs-container'>
+        <div className='cs-top'>
+          <a aria-label='go back to home' className='cs-logo' href='/home'>
             <img
-              className="cs-logo-img"
-              loading="lazy"
-              decoding="async"
+              className='cs-logo-img'
+              loading='lazy'
+              decoding='async'
               src={logo}
-              alt="logo"
-              width="209"
-              height="40"
+              alt='logo'
+              width='209'
+              height='40'
             />
           </a>
-          <ul className="cs-ul">
-            <li className="cs-li">
-              <a href="/home" className="cs-link">Home</a>
+          <ul className='cs-ul'>
+            <li className='cs-li'>
+              <a href='/home' className='cs-link'>
+                Home
+              </a>
             </li>
-            <li className="cs-li">
-              <a href="/about" className="cs-link">About</a>
+            <li className='cs-li'>
+              <a href='/about' className='cs-link'>
+                About
+              </a>
             </li>
-            <li className="cs-li">
-              <a href="/services" className="cs-link">Services</a>
+            <li className='cs-li'>
+              <a href='/services' className='cs-link'>
+                Services
+              </a>
             </li>
-            <li className="cs-li">
-              <a href="/contact-us" className="cs-link">Contact</a>
+            <li className='cs-li'>
+              <a href='/contact-us' className='cs-link'>
+                Contact
+              </a>
             </li>
           </ul>
         </div>
-        <div className="cs-bottom">
-          <ul className="cs-social">
-            <li className="cs-social-li">
-              <a href="https://www.facebook.com/profile.php?id=100038914455783" className="cs-social-link" aria-label="facebook" target="_blank" rel="noopener">
+        <div className='cs-bottom'>
+          <ul className='cs-social'>
+            <li className='cs-social-li'>
+              <a
+                href='https://www.facebook.com/profile.php?id=100038914455783'
+                className='cs-social-link'
+                aria-label='facebook'
+                target='_blank'
+                rel='noopener'>
                 <img
-                  className="cs-social-icon cs-default"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/face-white1.svg"
-                  alt="icon"
-                  loading="lazy"
-                  decoding="async"
-                  width="12"
-                  height="12"
-                  aria-hidden="true"
+                  className='cs-social-icon cs-default'
+                  src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/face-white1.svg'
+                  alt='icon'
+                  loading='lazy'
+                  decoding='async'
+                  width='12'
+                  height='12'
+                  aria-hidden='true'
                 />
               </a>
             </li>
-            <li className="cs-social-li">
-              <a href="https://www.facebook.com/profile.php?id=100038914455783" className="cs-social-link" aria-label="instagram" target="_blank" rel="noopener">
+            <li className='cs-social-li'>
+              <a
+                href='https://www.facebook.com/profile.php?id=100038914455783'
+                className='cs-social-link'
+                aria-label='instagram'
+                target='_blank'
+                rel='noopener'>
                 <img
-                  className="cs-social-icon cs-default"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/instagram.svg"
-                  alt="icon"
-                  loading="lazy"
-                  decoding="async"
-                  width="12"
-                  height="12"
-                  aria-hidden="true"
+                  className='cs-social-icon cs-default'
+                  src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/instagram.svg'
+                  alt='icon'
+                  loading='lazy'
+                  decoding='async'
+                  width='12'
+                  height='12'
+                  aria-hidden='true'
                 />
               </a>
             </li>
           </ul>
-          <span className="cs-copyright">
-            © Copyright 2024 - <a href="/home" className="cs-copyright-link">Grey Maverick Web Designs</a>
+          <span className='cs-copyright'>
+            © Copyright 2024 -{' '}
+            <a href='/home' className='cs-copyright-link'>
+              Grey Maverick Web Designs
+            </a>
           </span>
-          <div className="cs-flex">
-
-          </div>
+          <div className='cs-flex'></div>
         </div>
       </div>
     </footer>
   );
-}
+};
 
 export { ContactSectionFooter, Footer };
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../images/Lolito.webp';
-import { useTranslation } from 'react-i18next';  // Import useTranslation hook
+import logo from '../images/EDUARDO.svg';
+import { useTranslation } from 'react-i18next'; // Import useTranslation hook
 import './Navbar.css';
 
 const NavBar = () => {
-  const { t } = useTranslation();  // Get t function for translations
+  const { t } = useTranslation(); // Get t function for translations
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState({});
 
@@ -26,101 +26,103 @@ const NavBar = () => {
   };
 
   return (
-    <header id="cs-navigation" className={isMenuOpen ? "cs-active" : ""}>
-      <div className="cs-top-bar">
-        <div className="cs-top-container">
-          <div className="cs-top-contact">
-            <a href="tel:+17865456534" className="cs-top-link">
+    <header id='cs-navigation' className={isMenuOpen ? 'cs-active' : ''}>
+      <div className='cs-top-bar'>
+        <div className='cs-top-container'>
+          <div className='cs-top-contact'>
+            <a href='tel:+17865456534' className='cs-top-link'>
               <img
-                className="cs-link-icon"
-                src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/phone-stroke-white.svg"
-                alt="logo"
-                width="16"
-                height="16"
-                aria-hidden="true"
+                className='cs-link-icon'
+                src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/phone-stroke-white.svg'
+                alt='logo'
+                width='16'
+                height='16'
+                aria-hidden='true'
               />
               {t('callUs')}
             </a>
-            <a href="/home" className="cs-top-link">
+            <a href='/home' className='cs-top-link'>
               <img
-                className="cs-link-icon"
-                src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/clock-stroke-white.svg"
-                alt="logo"
-                width="20"
-                height="20"
-                aria-hidden="true"
+                className='cs-link-icon'
+                src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/clock-stroke-white.svg'
+                alt='logo'
+                width='20'
+                height='20'
+                aria-hidden='true'
               />
               {t('openingHours')}
             </a>
           </div>
-          <div className="cs-top-social">
-            <a href="https://www.facebook.com/profile.php?id=100038914455783" className="cs-social-link" target="blank">
+          <div className='cs-top-social'>
+            <a
+              href='https://www.facebook.com/profile.php?id=100038914455783'
+              className='cs-social-link'
+              target='blank'>
               <img
-                className="cs-social-icon"
-                src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/facebook-lgrey.svg"
-                alt="logo"
-                width="16"
-                height="16"
-                aria-hidden="true"
+                className='cs-social-icon'
+                src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/facebook-lgrey.svg'
+                alt='logo'
+                width='16'
+                height='16'
+                aria-hidden='true'
               />
             </a>
-            <a href="https://www.facebook.com/profile.php?id=100038914455783" className="cs-social-link" target='blank'>
+            <a
+              href='https://www.facebook.com/profile.php?id=100038914455783'
+              className='cs-social-link'
+              target='blank'>
               <img
-                className="cs-social-icon"
-                src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/instagram-lgrey.svg"
-                alt="logo"
-                width="16"
-                height="16"
-                aria-hidden="true"
+                className='cs-social-icon'
+                src='https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/instagram-lgrey.svg'
+                alt='logo'
+                width='16'
+                height='16'
+                aria-hidden='true'
               />
             </a>
           </div>
         </div>
       </div>
-      <div className="cs-container">
-        <a href="/home" className="cs-logo" aria-label="back to home">
+      <div className='cs-container'>
+        <a href='/home' className='cs-logo' aria-label='back to home'>
           <img
             src={logo}
-            alt="logo"
-            width="197"
-            height="43"
-            aria-hidden="true"
+            alt='logo'
+            width='197'
+            height='43'
+            aria-hidden='true'
           />
         </a>
-        <nav className="cs-nav" role="navigation">
+        <nav className='cs-nav' role='navigation'>
           <button
-            className={`cs-toggle ${isMenuOpen ? "cs-active" : ""}`}
-            aria-label="mobile menu toggle"
-            onClick={toggleMenu}
-          >
-            <div className="cs-box" aria-hidden="true">
-              <span className="cs-line cs-line1" aria-hidden="true"></span>
-              <span className="cs-line cs-line2" aria-hidden="true"></span>
-              <span className="cs-line cs-line3" aria-hidden="true"></span>
+            className={`cs-toggle ${isMenuOpen ? 'cs-active' : ''}`}
+            aria-label='mobile menu toggle'
+            onClick={toggleMenu}>
+            <div className='cs-box' aria-hidden='true'>
+              <span className='cs-line cs-line1' aria-hidden='true'></span>
+              <span className='cs-line cs-line2' aria-hidden='true'></span>
+              <span className='cs-line cs-line3' aria-hidden='true'></span>
             </div>
           </button>
-          <div className="cs-ul-wrapper">
+          <div className='cs-ul-wrapper'>
             <ul
-              id="cs-expanded"
-              className="cs-ul"
-              aria-expanded={isMenuOpen ? "true" : "false"}
-            >
-              {["Home", "About Us", "Services", "Contact Us", "Our Work"].map(
+              id='cs-expanded'
+              className='cs-ul'
+              aria-expanded={isMenuOpen ? 'true' : 'false'}>
+              {['Home', 'About Us', 'Services', 'Contact Us', 'Our Work'].map(
                 (item, index) => (
                   <li
                     key={index}
                     className={`cs-li ${
-                      isDropdownOpen[index] ? "cs-active" : ""
+                      isDropdownOpen[index] ? 'cs-active' : ''
                     }`}
                     onClick={() => {
                       toggleDropdown(index);
                       handleLinkClick();
-                    }}
-                  >
+                    }}>
                     <Link
                       to={`/${item.toLowerCase().replace(/\s+/g, '-')}`} // Dynamically generate route
-                      className="cs-li-link"
-                    >
+                      className='cs-li-link'>
                       {t(item)} {/* Translate tab names */}
                     </Link>
                   </li>
@@ -129,7 +131,7 @@ const NavBar = () => {
             </ul>
           </div>
         </nav>
-        <a href="/BookAnAppointment" className="cs-button-solid cs-nav-button">
+        <a href='/BookAnAppointment' className='cs-button-solid cs-nav-button'>
           {t('bookOnline')} {/* Translate the button */}
         </a>
       </div>
